8c378471b32e4bb7c6e4907fa852188ab41b2d46
Khanh Nguyen <khanh@rnd.works>
2017-09-04 09:21:24 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className = "container one">
+          <h1>DJI - SPARK</h1>
+          <p>A drone that designed for everyone,especially for you</p>
+          <p>Making your own breath taking video,</p>
+          <p>like a Pro</p>
+          <div className = "buynow">
+            <p>Buy it now</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default App;
